32977ed79118622478c18788702c4d46bd922d3e
ShantanuShirpure <shantanu.shirpure@live.com>
2018-03-02 09:34:35 -0500

    SearchBar container created

---
+++ src/containers/search_bar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+export default class SearchBar  extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state ={ term  : ''};
+    this.onInputChange  = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event){
+    this.setState({ term  : event.target.value})
+  }
+
+  onSubmitForm(event){
+    event.preventDefault();
+  }
+
+
+render(){
+  return(
+
+    <form  onSubmit={this.onSubmitForm} className="input-group">
+    <input
+      placeholder="Get a five-day forcaste for the Cities you search"
+      className="form-control"
+      value={this.state.term}
+      onChange={this.onInputChange} />
+
+
+      <span className="input-group-btn">
+        <button type="submit" className="btn btn-secondary">Submit</button>
+      </span>
+      </form>
+  );
+}
+}
